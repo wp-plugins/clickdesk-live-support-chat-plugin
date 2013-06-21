316bcf1400d9be5ecf857f5f6d94f5ee9ca3b7fc
jagan.2desk <jagan.2desk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-21 02:46:10 +0000

    Chat within wordpress dashboard
    git-svn-id: https://plugins.svn.wordpress.org/clickdesk-live-support-chat-plugin/tags/4.2@729167 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ cd-authentication.php
@@ -0,0 +1,33 @@
+<html>
+<head>  
+   <link rel="stylesheet" href="http://clickdesk.com/bootstrap.min.css">
+	<link rel="stylesheet" href="http://clickdesk.com/clickdesk.css">
+	<style type="text/css">
+	body{ padding-top:0px !important;}
+	</style>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript" ></script>
+<script src="http://twitter.github.com/bootstrap/1.4.0/bootstrap-twipsy.js" type="text/javascript" ></script>
+<script src="http://twitter.github.com/bootstrap/1.4.0/bootstrap-modal.js" type="text/javascript" ></script>
+<script src="http://twitter.github.com/bootstrap/assets/js/bootstrap-dropdown.js" type="text/javascript" ></script>
+
+
+</head>
+
+<body>
+
+<div class="container">
+<header>
+<br />
+<h1 class="ac">Webchat Panel is not active.<br/>
+<small>Sorry, it seems that you do not have an account yet. Please setup your ClickDesk account first</small></h1></br/>
+</header>
+
+<br /><br />
+<div class="ac">
+<a href="admin.php?page=livily_dashboard"><button class="btn success huge"> <b>Go to Account Setup</b></button></a>
+</div>
+</div>
+<br />
+	
+</body>
+</html>
