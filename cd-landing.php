306899607239c6cb3e9cb8994a3d6e8e58bcb07d
jagan.2desk <jagan.2desk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-24 13:43:37 +0000

    tagging version 4.3
    git-svn-id: https://plugins.svn.wordpress.org/clickdesk-live-support-chat-plugin/tags/4.3@844529 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ cd-landing.php
@@ -0,0 +1,34 @@
+<html>
+<head>  
+   <link rel="stylesheet" href="https://www.clickdesk.com/assets/plugin-files/bootstrap.min.css">
+   <link rel="stylesheet" href="https://www.clickdesk.com/assets/plugin-files/clickdesk.css">
+  
+	<style type="text/css">
+	body{ padding-top:0px !important;}
+	</style>
+
+</head>
+
+<body>
+
+<div class="container">
+<header>
+<br />
+<h1 class="ac">Engage visitors online on your Wordpress site now!<br/>
+<small>One click install to get live chat, voice chat, help desk & social toolbar on your wordpress site</small></h1></br/>
+</header>
+		<div class="row">
+			<div class="span6 offset1"><img class='ac' src="https://www.clickdesk.com/assets/plugin-files/cd-wp-landing.png" /></div>
+			<div class="span8 offset1"><iframe width="420" height="295" src="http://www.youtube.com/embed/4Yiyaz0k8yk" frameborder="0" allowfullscreen></iframe>
+			</div>
+		</div>
+
+<br /><br />
+<div class="ac">
+<a href="admin.php?page=livily_dashboard"><button class="btn success huge"> <b>Go to ClickDesk Dashboard</b></button></a>
+</div>
+</div>
+<br />
+	
+</body>
+</html>
