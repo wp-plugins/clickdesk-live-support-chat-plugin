46f52ccb74fe81c76d2b55f0c8f5d1bd7d186e6b
jagan.2desk <jagan.2desk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-25 07:33:56 +0000

    Updated image in landing page
    git-svn-id: https://plugins.svn.wordpress.org/clickdesk-live-support-chat-plugin/tags/4.3@938299 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cd-landing.php
+++ cd-landing.php
@@ -1,8 +1,8 @@
 <html>
-<head>  
+<head>
    <link rel="stylesheet" href="https://www.clickdesk.com/assets/plugin-files/bootstrap.min.css">
    <link rel="stylesheet" href="https://www.clickdesk.com/assets/plugin-files/clickdesk.css">
-  
+
 	<style type="text/css">
 	body{ padding-top:0px !important;}
 	</style>
@@ -14,21 +14,24 @@
 <div class="container">
 <header>
 <br />
-<h1 class="ac">Engage visitors online on your Wordpress site now!<br/>
-<small>One click install to get live chat, voice chat, help desk & social toolbar on your wordpress site</small></h1></br/>
+<h1 class="ac" style="margin-bottom:0px;">Live Chat, Video & VoIP, Helpdesk in one system<br/>
+<small>Increase conversions and keep more customers happy with one click ClickDesk installation.</small></h1></br/>
 </header>
 		<div class="row">
-			<div class="span6 offset1"><img class='ac' src="https://www.clickdesk.com/assets/plugin-files/cd-wp-landing.png" /></div>
-			<div class="span8 offset1"><iframe width="420" height="295" src="http://www.youtube.com/embed/4Yiyaz0k8yk" frameborder="0" allowfullscreen></iframe>
+			<!--<div class="span6 offset1"><img class='ac' src="https://www.clickdesk.com/assets/plugin-files/cd-wp-landing.png" /></div> -->
+			<div class="span10 offset1"><a href=""
+   onclick="window.open('http://www.youtube.com/embed/4Yiyaz0k8yk', 'ClickDesk video', 'toolbar=no,location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=550, height=450');"
+   class="uline" >
+   <img class='ac' src="https://www.clickdesk.com/assets/plugin-files/livechat-combo.png" /> </a>
 			</div>
 		</div>
 
-<br /><br />
+<br />
 <div class="ac">
 <a href="admin.php?page=livily_dashboard"><button class="btn success huge"> <b>Go to ClickDesk Dashboard</b></button></a>
 </div>
 </div>
 <br />
-	
+
 </body>
 </html>
